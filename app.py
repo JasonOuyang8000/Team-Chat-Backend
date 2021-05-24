43b03dd4e9fcdc5e5b0d20fa217271cb3f7c2ec4
Jason Ouyang <jasonouyang8000@gmail.com>
2021-05-24 14:29:49 -0700

    server

--- app.py
+++ app.py
@@ -112,7 +112,7 @@ def message(data):
     user.messages.append(message)
 
     models.db.session.commit()
-
+    print(message.created)
     print(message.to_json())
 
     emit('channel message', {'message': message.to_json()}, room=str(id))
