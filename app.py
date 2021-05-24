55c9403c2a2898b7a73ff70e30a02d0034a4d305
Jason Ouyang <jasonouyang8000@gmail.com>
2021-05-24 12:13:54 -0700

    proc file

--- app.py
+++ app.py
@@ -19,8 +19,8 @@ CORS(app)
 
 uri = os.getenv("DATABASE_URL")
 
-if uri.startswith("postgres://"):     
-  uri = uri.replace("postgres://", "postgresql://", 1)
+# if uri.startswith("postgres://"):     
+#   uri = uri.replace("postgres://", "postgresql://", 1)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = uri
 app.config['SQLALCHEMY_ECHO'] = False
